fe57d2ade7b7401d9a4f3096e83c8ea7381fec10
Cschmi22 <cschimi14@gmail.com>
2024-10-16 13:43:47 -0400

    Declaring, Initializing, and Interpolating Variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Declaring Variables
+            string name;
+            int age;
+            char middleInitial;
+            bool isOver18;
+            double currentTemp;
+            decimal price;
+
+            //Initialize Variables
+            name = "Caleb";
+            age = 29;
+            middleInitial = 'T';
+            if (age >= 18) 
+            { isOver18 = true; }
+            else { isOver18 = false; }
+            currentTemp = 64.3;
+            price = 19.95m;
+
+
+            //Interpolate
+            Console.WriteLine($"Name: {name} \n" +
+                              $"Age: {age} \n" +
+                              $"Middle Initial: {middleInitial} \n" +
+                              $"Over 18: {isOver18} \n" +
+                              $"Current Temperature: {currentTemp} \n" +
+                              $"Price: {price}");
+
+
         }
     }
 }
